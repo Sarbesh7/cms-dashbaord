05b5c8a14fbb6f497e852a6037cd0d93e454f9e0
Swikrity AC <swikrityac31@gmail.com>
2026-06-06 22:18:41 +0545

    worked with errors

--- apps/events/views.py
+++ apps/events/views.py
@@ -12,26 +12,20 @@ from .pagination import EventPagination
 class EvenListtView(APIView):
     def get(self,request):
         events = Event.objects.all()
+
         search = request.query_params.get("search")
-        status_filter = request.query_params.get('status')
+        status_filter = request.query_params.get("status")
+
         if search:
             events = events.filter(title__icontains=search)
         if status_filter:
             events = events.filter(status=status_filter)
 
-        serializer = EventSerializer(events,many=True)
-        return Response(serializer.data,status=status.HTTP_200_OK)
-    
-
-    def get(self,request):
-        events=Event.objects.all()
         paginator = EventPagination()
-        result_page = paginator.paginate_queryset(events,request)
-        serializer = EventSerializer(result_page,many=True)
-        return paginator.get_paginated_response(
-            serializer.data
-        )
+        result_page = paginator.paginate_queryset(events,request)    
 
+        serializer = EventSerializer(result_page,many=True)
+        return paginator.get_paginated_response(serializer.data)
 
     def post(self,request):
         serializer = EventSerializer(data=request.data)
@@ -43,21 +37,21 @@ class EvenListtView(APIView):
 
 class EventDetailsView(APIView) :
     def get(self,request,slug) :
-        event = get_object_or_404(Event,slug=slug)
+        event = get_object_or_404(Event,slug__iexact=slug)
         serializer = EventSerializer(event)
         return Response(serializer.data,status=status.HTTP_200_OK)
     
 
     def put(self,request,slug):
-        event = get_object_or_404(Event,slug=slug)
+        event = get_object_or_404(Event,slug__iexact=slug)
         serializer = EventSerializer(event,data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data,status=status.HTTP_200_OK)
-        return Response(serializer.errors,status=status.HTTP_204_NO_CONTENT)
+        return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
     
     def delete(self,request,slug):
-        event= get_object_or_404(Event,slug=slug)
+        event= get_object_or_404(Event,slug__iexact=slug)
         event.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
 
