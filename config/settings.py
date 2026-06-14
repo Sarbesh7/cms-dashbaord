5dde5a81f58efc1f98ae4d48d8faffa3f270b737
Sarbesh Adhikari <sstudio985@gmail.com>
2026-06-14 21:47:32 +0545

    cors error

--- config/settings.py
+++ config/settings.py
@@ -49,11 +49,11 @@ INSTALLED_APPS = [
     'apps.core',
     'apps.papers',
     'rest_framework.authtoken',
-    'corsheaders',
+    # 'corsheaders',
 ]
 
 MIDDLEWARE = [
-    'corsheaders.middleware.CorsMiddleware',
+    # 'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -165,7 +165,7 @@ MEDIA_URL = 'media/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3000",
-    "http://127.0.0.1:3000",
-]
+# CORS_ALLOWED_ORIGINS = [
+#     "http://localhost:3000",
+#     "http://127.0.0.1:3000",
+# ]
