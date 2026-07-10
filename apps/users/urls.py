15171656777e7e5010e2e85f3470632b4e67cb3d
Sarbesh Adhikari <sstudio985@gmail.com>
2026-07-10 23:37:31 +0545

    errors

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from .views import LoginView,UserView,ChangePasswordView,LogoutView,ResetPasswordView
-from .views import LoginView,UserView,ChangePasswordView,TestEmailView,ForgotPasswordView,ResetPasswordView,UserDetailView
+from .views import LoginView,UserView,ChangePasswordView,ResetPasswordView,UserDetailView
 
 
 
@@ -9,8 +9,8 @@ urlpatterns = [
     path('api/v1/users/',UserView.as_view()),
     path('api/v1/users/<int:user_id>/', UserDetailView.as_view()),
     path('api/v1/users/change-password/',ChangePasswordView.as_view()),
-    path("api/v1/test-email/", TestEmailView.as_view()),
-    path("api/v1/users/forgot-password/", ForgotPasswordView.as_view()),
+    # path("api/v1/test-email/", TestEmailView.as_view()),
+    # path("api/v1/users/forgot-password/", ForgotPasswordView.as_view()),
     path("api/v1/users/reset-password/", ResetPasswordView.as_view()),
     path("api/v1/users/logout/", LogoutView.as_view()),
     
