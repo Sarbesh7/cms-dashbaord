2e3421d5d0c5f6c05878415df019e5115d50a076
Sarbesh Adhikari <sstudio985@gmail.com>
2026-07-13 00:08:44 +0545

    events

--- apps/events/models.py
+++ apps/events/models.py
@@ -23,12 +23,42 @@ class SecureFilePath:
         return os.path.join(self.folder_name, secure_name)
 
 
+from django.db import models
+from django.utils.text import slugify
+
+
 class Event(TimeStampModel):
-    title = models.CharField(max_length=200)
-    slug = models.SlugField(unique=True, blank=True ,db_index=True)
-    description = models.TextField()
+    class Category(models.TextChoices):
+        WORKSHOP = 'workshop', 'Workshop'
+        SEMINAR = 'seminar', 'Seminar'
+        CONFERENCE = 'conference', 'Conference'
+        WEBINAR = 'webinar', 'Webinar'
+        HACKATHON = 'hackathon', 'Hackathon'
+        BOOTCAMP = 'bootcamp', 'Bootcamp'
+        TALK = 'talk', 'Talk'
+        MOCK_TEST = 'mocktest', 'Mock Test'
+        OTHER = 'other', 'Other'
+
+    class Tag(models.TextChoices):
+        WEB = 'web', 'Web'
+        BEGINNER = 'beginner', 'Beginner'
+        INTERMEDIATE = 'intermediate', 'Intermediate'
+        FRONTEND = 'frontend', 'Front-End'
+        BACKEND = 'backend', 'Back-End'
+
+    class Status(models.TextChoices):
+        DRAFT = 'draft', 'Draft'
+        PUBLISHED = 'published', 'Published'
+        COMPLETED = 'completed', 'Completed'
+
     
-    # Securely point to events_templates subfolder
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(unique=True, blank=True, db_index=True)
+    description = models.TextField()
+    organiser = models.CharField(max_length=200, blank=True, null=True)
+    location = models.CharField(max_length=200, blank=True, null=True)
+    
+    
     image = models.FileField(
         upload_to=SecureFilePath('events_templates/'),
         validators=[validate_file_size],
@@ -36,18 +66,22 @@ class Event(TimeStampModel):
         blank=True
     )
     
+    
     date = models.DateTimeField()
+    start_time = models.DateTimeField(blank=True, null=True)
+    end_time = models.DateTimeField(blank=True, null=True)
+    
+   
+    available_seats = models.PositiveIntegerField(default=0)
+    registration_fee_bmc = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
+    registration_fee_non_bmc = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
     registration_link = models.URLField(blank=True, null=True)
+    
+    
+    category = models.CharField(max_length=50, choices=Category.choices, null=True, blank=True)
+    tags = models.CharField(max_length=50, choices=Tag.choices, null=True, blank=True)
+    status = models.CharField(max_length=20, choices=Status.choices, default=Status.DRAFT)
     mentors = models.ManyToManyField('Mentor', related_name='events', blank=True)
-    status = models.CharField(
-        max_length=20,
-        choices=[
-            ('draft', 'Draft'),
-            ('published', 'Published'),  
-            ('completed', 'Completed')
-        ],
-        default='draft'
-    )
 
     def __str__(self):
         return self.title
@@ -56,8 +90,7 @@ class Event(TimeStampModel):
         if not self.slug:
             self.slug = slugify(self.title)
         super().save(*args, **kwargs)
-
-
+   
 class Mentor(TimeStampModel):
     name = models.CharField(max_length=100)
     email = models.EmailField(unique=True)
@@ -65,7 +98,7 @@ class Mentor(TimeStampModel):
     expertise = models.CharField(max_length=200)
     linkedin_profile = models.URLField(blank=True, null=True)
     
-    # Securely point to mentors_photos subfolder
+    
     photo = models.ImageField(
         upload_to=SecureFilePath('mentors_photos/'),
         validators=[validate_file_size],
