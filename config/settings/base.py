9dea7c32767e2056d33885dd030a407f2835b1e5
Sarbesh Adhikari <sstudio985@gmail.com>
2026-08-04 21:32:28 +0545

    cors

--- config/settings/base.py
+++ config/settings/base.py
@@ -76,7 +76,14 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'config.urls'
-CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+]
+CSRF_TRUSTED_ORIGINS = [
+    "http://localhost:3000",
+]
+CORS_ALLOW_CREDENTIALS = True
+
 
 TEMPLATES = [
     {
